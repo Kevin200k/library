df35bef16189a2a94ddf1505ce7dfac007d4c4ae
kevin200k <mikimiki2194@gmail.com>
2024-03-08 09:03:22 +0100

    Replaced the checkbox with a dropdown menu

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let toggleDialogue = document.getElementById("toggleDialogue");
 let Dialogue = document.getElementById("Dialogue")
 let closeDialogue = document.getElementById("closeDialogue")
-
+let body = document.querySelector("body");
 //Show Dialogue
 toggleDialogue.addEventListener("click", function(){
     Dialogue.showModal();
@@ -19,33 +19,83 @@ let readField = document.getElementById("read");
 let submit = document.getElementById("submit");
 
 //Defining card and its contents
-let cardContainer= document.getElementById("card");
-let titleValue = document.getElementById("titleValue");
-let authorValue = document.getElementById("authorValue");
-let pagesValue = document.getElementById("pagesValue");
-let readValue = document.getElementById("readValue");
+let cardContainer= document.querySelector(".card");
+let titleValue = document.querySelector(".titleValue");
+let authorValue = document.querySelector(".authorValue");
+let pagesValue = document.querySelector(".pagesValue");
+let readValue = document.querySelector(".readValue");
 
 
 submit.addEventListener("click", function(){
-   if(!(card.title) === '' && !(card.author) === '' && !(card.pages) === ''){
+    //Constructor function for Libary
     function Book(title, author, pages, read) {
-         this.title = title;
-         this.author = author;
-         this.pages = pages;
-         this.read = read;
-       }
-       
-       var card = new Book(titleField.value, authorField.value, pagesField.value, readField.value)
-       
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = (read == "1") ? "Read" : "Not Read";
     }
-    console.log(card.title === "null")
+    Book();
+    
+    let card = new Book(titleField.value, authorField.value, pagesField.value, readField.value)
+    // console.log(card)
 
+    // titleValue.innerText = card.title;
+    // authorValue.innerText = card.author;
+    // pagesValue.innerText = card.pages;
+    // readValue.innerText = card.read;
+    
+    function addBookToLibrary(){
+        if(card.title === "" ||
+           card.author === "" ||
+           card.pages === ""){
+                return null
+           }else{
+                myLibrary.push(card)
+           }
+        
+    }
+    addBookToLibrary();
+    function loopOver() {
+        for (let i = 0; i < myLibrary.length; i++) {
+            let j = myLibrary.lastIndexOf(card)
+            console.log(i);
+            console.log(myLibrary);
+            console.log(j) 
+
+            let cardContainer1 = document.createElement("div");
+            cardContainer1.setAttribute("class", "cardContainer");
+            let titleValue1 = document.createElement("h2");
+            titleValue1.setAttribute("class", "titleValue");
+            let authorValue1 = document.createElement("p");
+            authorValue1.setAttribute("class", "authorValue");
+            let pagesValue1 = document.createElement("p");
+            pagesValue1.setAttribute("class", "pagesValue");
+            let readValue1 = document.createElement("p");
+            readValue1.setAttribute("class", "readValue");
+            
+            titleValue1.innerText = card.title;
+            authorValue1.innerText = card.author;
+            pagesValue1.innerText = card.pages;
+            readValue1.innerText = card.read;
+            
+            body.appendChild(cardContainer1);
+            cardContainer1.appendChild(titleValue1);
+            cardContainer1.appendChild(authorValue1);
+            cardContainer1.appendChild(pagesValue1);
+            cardContainer1.appendChild(readValue1);
+            
+            
+            break
+        }
+        
+    }
+    loopOver();
+
+
+    console.log(myLibrary[0])
+    // titleField.value = "";
+    // authorField.value = "";
+    // pagesField.value = ""
+    // readField.value = ""
 });
 const myLibrary = [];
-
-//Constructor function for the library
-
-// //Function to add created object to array
-// function addBookToLibrary() {
-//     myLibrary = myLibrary.push(card)
-// }
