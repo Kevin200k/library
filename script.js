b3fd2301295c403037533dbee8b79c6c2fe62062
kevin200k <mikimiki2194@gmail.com>
2024-03-08 08:50:51 +0100

    Added functions to submit button to preview the dialog on the page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let toggleDialogue = document.getElementById("toggleDialogue");
 let Dialogue = document.getElementById("Dialogue")
-let closeDialogue = document.getElementById("openDialogue")
+let closeDialogue = document.getElementById("closeDialogue")
 
 //Show Dialogue
 toggleDialogue.addEventListener("click", function(){
@@ -12,8 +12,40 @@ closeDialogue.addEventListener("click", function(){
 })
 
 //Defining form fields
-let title = document.getElementById("title");
-let author = document.getElementById("author");
-let pages = document.getElementById("pages");
-let read = document.getElementById("read");
+let titleField = document.getElementById("title");
+let authorField = document.getElementById("author");
+let pagesField = document.getElementById("pages");
+let readField = document.getElementById("read");
+let submit = document.getElementById("submit");
 
+//Defining card and its contents
+let cardContainer= document.getElementById("card");
+let titleValue = document.getElementById("titleValue");
+let authorValue = document.getElementById("authorValue");
+let pagesValue = document.getElementById("pagesValue");
+let readValue = document.getElementById("readValue");
+
+
+submit.addEventListener("click", function(){
+   if(!(card.title) === '' && !(card.author) === '' && !(card.pages) === ''){
+    function Book(title, author, pages, read) {
+         this.title = title;
+         this.author = author;
+         this.pages = pages;
+         this.read = read;
+       }
+       
+       var card = new Book(titleField.value, authorField.value, pagesField.value, readField.value)
+       
+    }
+    console.log(card.title === "null")
+
+});
+const myLibrary = [];
+
+//Constructor function for the library
+
+// //Function to add created object to array
+// function addBookToLibrary() {
+//     myLibrary = myLibrary.push(card)
+// }
