6c7d7b5a350480174cea40c82e0c3c629c0f1c60
kevin200k <mikimiki2194@gmail.com>
2024-04-20 21:52:35 +0100

    Basic layout for javascript lined up with a class for the different fields

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+class Book{
+    constructor(name, author, read, pages){
+        this.name = name;
+        this.author = author;
+        this.read = read;
+        this.pages = pages;
+    }
+}
+function addBookToLibrary() {
+  // do stuff here
+}
