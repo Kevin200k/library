15fc964f0ae69c59f41fb92ebc75a90e5cf6de0f
kevin200k <mikimiki2194@gmail.com>
2024-04-20 22:51:10 +0100

    Show and hide dialog form to enter library details

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// DOM Elements
+let dialogForm = document.getElementById("form_dialog");
+let closeForm = document.getElementById("close_button");
+let addBook = document.getElementById("add_book");
+
+//Functions to show and hide dialog
+let showDialog = () => {
+    dialogForm.showModal()
+}
+
+let hideDialog = () => {
+    dialogForm.close()
+}
+
+//Event listeners to show and hide dialog form
+addBook.addEventListener("click", showDialog);
+closeForm.addEventListener("click", hideDialog);
+
 const myLibrary = [];
 
 class Book{
